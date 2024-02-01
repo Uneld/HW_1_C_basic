9a43696d48ffc11fc243098cc6436e5a58a1ea6c
Uneld <aleksey.minchenya@gmail.com>
2024-02-01 22:26:13 +0300

    first commit

---
+++ homework1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    // Задание 1
+    printf("Task 1\n");
+    printf("----------------------------------\n");
+    printf("Hello world!\n");
+	printf("----------------------------------\n\n");
+    
+    
+    // Задание 2
+    printf("Task 2\n");
+    printf("----------------------------------\n");
+    printf("Let's\n");
+    printf("   go\n");
+    printf("    to walk\n");
+	printf("----------------------------------\n\n");
+
+
+    // Задание 3
+    printf("Task 3\n");
+    printf("----------------------------------\n");
+    printf("   *\n");
+    printf("  ***\n");
+    printf(" *****\n");
+    printf("*******\n");
+    printf(" HH HH\n");
+    printf(" ZZZZZ\n");
+    printf("----------------------------------\n\n");
+
+    // Задание 4
+    printf("Task 4\n");
+    printf("----------------------------------\n");
+    printf(" __    __\n");
+    printf("/  \\__/  \\\n");
+    printf("\\__/  \\__/\n");
+    printf("/  \\__/  \\\n");
+    printf("\\__/  \\__/\n");
+ 
+    printf("----------------------------------\n\n");
+
+    return 0;
+}
